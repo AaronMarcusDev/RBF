82593c05594532581b9888c81da625874ff8ffc6
AaronMarcusDev <aaronmarcus2007@gmail.com>
2022-10-10 14:46:55 +0200

    INC - SHL & DEC -> SHR

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -6,20 +6,24 @@ pub fn interpret(tokens: Vec<lexer::Instruction>) {
 
     for command in tokens {
         match command {
-            lexer::Instruction::ShiftLeft => if ptr != 0 {ptr -= 1},
-            lexer::Instruction::ShiftRight => ptr += 1,
-            lexer::Instruction::Increment => {
-                if mem[ptr as usize] == (mem.len() - 1) as u8 {
-                    mem[ptr as usize] = 0;
+            lexer::Instruction::ShiftLeft => {
+                if ptr == 0 {
+                    ptr = mem.len() as u32;
                 } else {
-                    mem[ptr as usize] += 1;
+                    ptr -= 1;
                 }
             }
-            lexer::Instruction::Decrement => {
-                if mem[ptr as usize] == 0 {
-                    mem[ptr as usize] = (mem.len() - 1) as u8;
+            lexer::Instruction::ShiftRight => {
+                if ptr == mem.len() as u32 {
+                    ptr = 0;
                 } else {
-                    mem[ptr as usize] -= 1;
+                    ptr += 1;
+                }
+            }
+            lexer::Instruction::Increment => mem[ptr as usize] += 1,
+            lexer::Instruction::Decrement => {
+                if mem[ptr as usize] != 0 {
+                    mem[ptr as usize] -= 1
                 }
             }
             lexer::Instruction::CharIn => {
